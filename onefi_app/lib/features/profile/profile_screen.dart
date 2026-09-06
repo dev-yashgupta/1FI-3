63c6a838428822be4310494495357992ae051ae5
Yash <yashgupta9437@gmail.com>
2026-09-06 13:14:05 +0530

    fix(o): update profile_screen.dart

--- onefi_app/lib/features/profile/profile_screen.dart
+++ onefi_app/lib/features/profile/profile_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import '../../core/theme/app_colors.dart';
-import '../../core/constants/app_constants.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
